5c9c6c11b261d109ce2ac3d7f479bb37d804c915
Ollie Pugh <oliver.pugh@mac.com>
2021-10-12 17:45:58 +0100

    Added production configuration"

--- src/env/production.ts
+++ src/env/production.ts
@@ -1,9 +1,9 @@
 module.exports = {
     aws_region: "eu-west-1",
     db_endpoint: null,
-    ec_table_name: "something",
-    ec_email_index: "something_else",
-    responsibility_table_name: "another_store_name",
-    email_template_name: "something-template",
+    ec_table_name: "emergency-contact-store",
+    ec_email_index: "email-Index",
+    responsibility_table_name: "responsibility-store",
+    email_template_name: "ec-invite-template",
     email_source: "donotreply@safe-step.net"
 }
